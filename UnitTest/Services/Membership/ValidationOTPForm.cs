c1df2cdc92c7d971c59d238b61fe9be6b505f643
jordanmoonlay <jordan.leonardi@moonlay.com>
2018-04-24 15:40:27 +0700

    latest commit

---
+++ UnitTest/Services/Membership/ValidationOTPForm.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTest.Services.Membership
+{
+    internal class ValidationOTPForm
+    {
+        public string userName { get; set; }
+        public string token { get; set; }
+    }
+}
